597184b2e282f06563f70ac0536d70da9ca2939c
Kyle Cordell <kcordell-git@outlook.com>
2023-07-26 22:34:38 -0700

    Adds map image to CityDetails component

--- src/components/CityDetails.jsx
+++ src/components/CityDetails.jsx
@@ -6,6 +6,9 @@ export default class CityDetails extends React.Component {
   render() {
     const { currentCity } = this.props;
     const { display_name, lat, lon } = currentCity;
+    const mapSrc = `https://maps.locationiq.com/v3/staticmap?key=${
+      import.meta.env.VITE_LOCATIONIQ_API_KEY
+    }&center=${lat},${lon}&zoom=12&scale=2`;
     return (
       <>
         <Table borderless>
@@ -24,6 +27,11 @@ export default class CityDetails extends React.Component {
             </tr>
           </tbody>
         </Table>
+        <img
+          src={mapSrc}
+          alt={`Map of ${display_name}`}
+          className="object-fit-contain img-fluid mx-auto border rounded"
+        />
       </>
     );
   }

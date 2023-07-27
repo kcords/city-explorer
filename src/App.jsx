d4ca6ebcdd5fab2aad3a75ea85d3b89b78db0026
Kyle Cordell <kcordell-git@outlook.com>
2023-07-26 19:43:40 -0700

    Adds SearchBar to App with formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import SearchBar from "./components/SearchBar";
+
 export default class App extends React.Component {
-  renter() {
-    return <></>;
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col className="my-4">
+            <SearchBar />
+          </Col>
+        </Row>
+      </Container>
+    );
   }
 }

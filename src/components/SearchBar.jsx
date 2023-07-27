36140bae9a6f11ff6e70b64e656af370b954f788
Kyle Cordell <kcordell-git@outlook.com>
2023-07-26 21:02:43 -0700

    Updates SearchBar component to perform get request from LocationIQ api

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,20 +1,55 @@
 import React from "react";
+import axios from "axios";
 
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import Button from "react-bootstrap/Button";
 
 export default class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: "",
+    };
+  }
+
+  handleSearchInput = ({ target: { value: searchTerm } }) => {
+    this.setState({ searchTerm });
+  };
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const { setCurrentCity } = this.props;
+    const apiURL = `https://us1.locationiq.com/v1/search?key=${
+      import.meta.env.VITE_LOCATIONIQ_API_KEY
+    }&q=${this.state.searchTerm}&format=json`;
+    const {
+      data: [currentCity],
+    } = await axios.get(apiURL);
+    setCurrentCity(currentCity);
+  };
+
   render() {
+    const {
+      handleSearchInput,
+      handleSubmit,
+      state: { searchTerm },
+    } = this;
+
     return (
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <InputGroup>
           <InputGroup.Text>Search</InputGroup.Text>
           <Form.Control
             placeholder="Input your city search here..."
             aria-label="City search input"
+            onChange={handleSearchInput}
+            onKeyPress={({ key }) => key === "enter" && handleSubmit()}
+            value={searchTerm}
           />
-          <Button type="submit">Explore!</Button>
+          <Button type="submit" disabled={!searchTerm}>
+            {searchTerm ? "Explore!" : "Ready?"}
+          </Button>
         </InputGroup>
       </Form>
     );

4832177f0fb6570aec6a47fb9cccde5c2e0d79aa
Kyle Cordell <kcordell-git@outlook.com>
2023-07-29 18:41:08 -0700

    Adds Weather component to CityDetails

--- src/components/CityDetails.jsx
+++ src/components/CityDetails.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 import Table from "react-bootstrap/Table";
+import Weather from './Weather'
 
 export default class CityDetails extends React.Component {
   render() {
-    const { currentCity } = this.props;
-    const { display_name, lat, lon } = currentCity;
+    const { currentCity: { display_name, lat, lon } } = this.props;
+    const [ cityName ] = display_name.split(',')
     const mapSrc = `https://maps.locationiq.com/v3/staticmap?key=${
       import.meta.env.VITE_LOCATIONIQ_API_KEY
     }&center=${lat},${lon}&zoom=12&scale=2`;
@@ -27,6 +28,7 @@ export default class CityDetails extends React.Component {
             </tr>
           </tbody>
         </Table>
+        <Weather key={cityName} cityName={cityName} lat={lat} lon={lon} />
         <img
           src={mapSrc}
           alt={`Map of ${display_name}`}

7e37b31170dbd8ee2e5dcd3d22c887e69effab89
Kyle Cordell <kcordell-git@outlook.com>
2023-08-03 13:24:42 -0700

    Moves weather and movie fetching into City component

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,11 +1,56 @@
 import React from "react";
+import axios from "axios";
 
 import { CityDetails, Weather, Movies } from "./";
 
 export default class City extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weather: { forecasts: [], errorMessage: null },
+      movies: {
+        top: [],
+        errorMessage: null,
+      },
+    };
+  }
+
+  componentDidMount() {
+    this.getWeatherForecast();
+    this.getTopMovies();
+  }
+
+  getWeatherForecast = async () => {
+    try {
+      const { cityName, lat, lon } = this.props;
+      const url = `${
+        import.meta.env.VITE_SERVER_URL
+      }/weather?searchQuery=${cityName}&lat=${lat}&lon=${lon}`;
+
+      const { data: forecasts } = await axios.get(url);
+      this.setState({ weather: { forecasts } });
+    } catch ({ response: { statusText: errorMessage } }) {
+      this.setState({ weather: { errorMessage } });
+    }
+  };
+
+  getTopMovies = async () => {
+    try {
+      const { cityName } = this.props;
+      const url = `${
+        import.meta.env.VITE_SERVER_URL
+      }/movies?searchQuery=${cityName}`;
+      const { data: top } = await axios.get(url);
+      this.setState({ movies: { top } });
+    } catch ({ response: { statusText: errorMessage } }) {
+      this.setState({ movies: { errorMessage } });
+    }
+  };
+
   render() {
     const { currentCity } = this.props;
     const { display_name, lat, lon } = currentCity;
+    const { weather, movies } = this.state;
 
     const [cityName] = display_name.split(",");
 
@@ -18,16 +63,19 @@ export default class City extends React.Component {
         <CityDetails currentCity={currentCity} />
         <Weather
           key={`weather-${cityName}`}
-          cityName={cityName}
-          lat={lat}
-          lon={lon}
+          forecasts={weather.forecasts}
+          errorMessage={weather.errorMessage}
         />
         <img
           src={mapSrc}
           alt={`Map of ${display_name}`}
           className="object-fit-contain img-fluid mx-auto border shadow-sm rounded"
         />
-        <Movies key={`movies-${cityName}`} cityName={cityName} />
+        <Movies
+          key={`movies-${cityName}`}
+          movies={movies.top}
+          errorMessage={movies.errorMessage}
+        />
       </>
     );
   }

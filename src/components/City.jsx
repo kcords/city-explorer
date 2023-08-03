1ee803a8c6ba0da3dd424256510f734e15e55d28
Kyle Cordell <kcordell-git@outlook.com>
2023-08-03 15:08:24 -0700

    Updates City layout

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import axios from "axios";
 
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import { CityDetails, Weather, Movies } from "./";
 
 export default class City extends React.Component {
@@ -15,36 +17,45 @@ export default class City extends React.Component {
     };
   }
 
-  componentDidMount() {
-    this.getWeatherForecast();
-    this.getTopMovies();
-  }
+  componentDidMount = async () => {
+    const newState = {};
+    newState.weather = await this.getWeatherForecast();
+    newState.movies = await this.getTopMovies();
+    this.setState(newState);
+  };
 
   getWeatherForecast = async () => {
-    try {
-      const { cityName, lat, lon } = this.props;
-      const url = `${
-        import.meta.env.VITE_SERVER_URL
-      }/weather?searchQuery=${cityName}&lat=${lat}&lon=${lon}`;
+    const { display_name, lat, lon } = this.props.currentCity;
+    const [cityName] = display_name.split(",") || [display_name];
+    const url = `${
+      import.meta.env.VITE_SERVER_URL
+    }/weather?searchQuery=${cityName}&lat=${lat}&lon=${lon}`;
 
+    const weather = {};
+    try {
       const { data: forecasts } = await axios.get(url);
-      this.setState({ weather: { forecasts } });
+      weather.forecasts = forecasts;
     } catch ({ response: { statusText: errorMessage } }) {
-      this.setState({ weather: { errorMessage } });
+      weather.errorMessage = errorMessage;
     }
+    return weather;
   };
 
   getTopMovies = async () => {
+    const { display_name } = this.props.currentCity;
+    const [cityName] = display_name.split(",");
+    const url = `${
+      import.meta.env.VITE_SERVER_URL
+    }/movies?searchQuery=${cityName}`;
+
+    const movies = {};
     try {
-      const { cityName } = this.props;
-      const url = `${
-        import.meta.env.VITE_SERVER_URL
-      }/movies?searchQuery=${cityName}`;
       const { data: top } = await axios.get(url);
-      this.setState({ movies: { top } });
+      movies.top = top;
     } catch ({ response: { statusText: errorMessage } }) {
-      this.setState({ movies: { errorMessage } });
+      movies.errorMessage = errorMessage;
     }
+    return movies;
   };
 
   render() {
@@ -59,24 +70,29 @@ export default class City extends React.Component {
     }&center=${lat},${lon}&zoom=12&scale=2`;
 
     return (
-      <>
-        <CityDetails currentCity={currentCity} />
-        <Weather
-          key={`weather-${cityName}`}
-          forecasts={weather.forecasts}
-          errorMessage={weather.errorMessage}
-        />
-        <img
-          src={mapSrc}
-          alt={`Map of ${display_name}`}
-          className="object-fit-contain img-fluid mx-auto border shadow-sm rounded"
-        />
-        <Movies
-          key={`movies-${cityName}`}
-          movies={movies.top}
-          errorMessage={movies.errorMessage}
-        />
-      </>
+      <Row>
+        <Col md={5}>
+          <CityDetails currentCity={currentCity} />
+          <img
+            key={`map-${cityName}`}
+            src={mapSrc}
+            alt={`Map of ${display_name}`}
+            className="object-fit-contain img-fluid mx-auto border shadow-sm rounded"
+          />
+        </Col>
+        <Col lg={7}>
+          <Weather
+            key={`weather-${cityName}`}
+            forecasts={weather.forecasts}
+            errorMessage={weather.errorMessage}
+          />
+          <Movies
+            key={`movies-${cityName}`}
+            movies={movies.top}
+            errorMessage={movies.errorMessage}
+          />
+        </Col>
+      </Row>
     );
   }
 }

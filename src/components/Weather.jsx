b85c8b6a74a68f18f6234343ec286b55aa221b03
Kyle Cordell <kcordell-git@outlook.com>
2023-08-03 22:33:15 -0700

    Updates Weather component formatting

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import Card from "react-bootstrap/Card";
 import Table from "react-bootstrap/Table";
 
 export default class Weather extends React.Component {
@@ -7,45 +8,45 @@ export default class Weather extends React.Component {
     const { forecasts, errorMessage } = this.props;
     return (
       <>
-        <Table borderless striped className="p-3 shadow-sm rounded">
-          <thead>
-            <tr>
-              <th colSpan={2}>
-                <h3>7-day Forecast</h3>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {errorMessage || forecasts.length < 1 ? (
-              <tr key={errorMessage}>
-                <td colSpan={2} className="text-break text-wrap">
-                  {errorMessage || `No weather data available`}
-                </td>
-              </tr>
-            ) : (
-              forecasts.map(({ date, description }) => {
-                const dateObj = new Date(date);
-                const options = {
-                  weekday: "short",
-                  month: "short",
-                  day: "numeric",
-                  timeZone: "UTC",
-                };
-                const formattedDate = new Intl.DateTimeFormat(
-                  "en-US",
-                  options
-                ).format(dateObj);
-
-                return (
-                  <tr key={date + description}>
-                    <td className="text-break text-wrap">{formattedDate}</td>
-                    <td className="text-break text-wrap">{description}</td>
+        <h3>7-day Forecast</h3>
+        <Card>
+          <Card.Body>
+            <Table borderless striped className="p-3 shadow-sm rounded">
+              <tbody>
+                {errorMessage || forecasts.length < 1 ? (
+                  <tr key={errorMessage}>
+                    <td colSpan={2} className="text-break text-wrap">
+                      {errorMessage || `No weather data available`}
+                    </td>
                   </tr>
-                );
-              })
-            )}
-          </tbody>
-        </Table>
+                ) : (
+                  forecasts.map(({ date, description }) => {
+                    const dateObj = new Date(date);
+                    const options = {
+                      weekday: "short",
+                      month: "short",
+                      day: "numeric",
+                      timeZone: "UTC",
+                    };
+                    const formattedDate = new Intl.DateTimeFormat(
+                      "en-US",
+                      options
+                    ).format(dateObj);
+
+                    return (
+                      <tr key={date + description}>
+                        <td className="text-break text-wrap">
+                          {formattedDate}
+                        </td>
+                        <td className="text-break text-wrap">{description}</td>
+                      </tr>
+                    );
+                  })
+                )}
+              </tbody>
+            </Table>
+          </Card.Body>
+        </Card>
       </>
     );
   }

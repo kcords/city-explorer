e1bc627da9338266350e37564e1e3ea8d2f9aad5
Kyle Cordell <kcordell-git@outlook.com>
2023-08-03 22:33:01 -0700

    Updates CityDetails import

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 export { default as SearchBar } from "./SearchBar";
-export { default as City } from "./City";
 export { default as CityDetails } from "./CityDetails";
+export { default as CityMap } from "./CityMap";
 export { default as Weather } from "./Weather";
 export { default as Movies } from "./Movies";
-

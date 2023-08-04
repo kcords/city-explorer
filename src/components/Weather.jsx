cd9ab4ee4428c06f3582b113a4f5b529f8bdd2d3
Kyle Cordell <kcordell-git@outlook.com>
2023-08-03 22:50:21 -0700

    Removes Card from Weather component

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-import Card from "react-bootstrap/Card";
 import Table from "react-bootstrap/Table";
 
 export default class Weather extends React.Component {
@@ -9,44 +8,38 @@ export default class Weather extends React.Component {
     return (
       <>
         <h3>7-day Forecast</h3>
-        <Card>
-          <Card.Body>
-            <Table borderless striped className="p-3 shadow-sm rounded">
-              <tbody>
-                {errorMessage || forecasts.length < 1 ? (
-                  <tr key={errorMessage}>
-                    <td colSpan={2} className="text-break text-wrap">
-                      {errorMessage || `No weather data available`}
-                    </td>
-                  </tr>
-                ) : (
-                  forecasts.map(({ date, description }) => {
-                    const dateObj = new Date(date);
-                    const options = {
-                      weekday: "short",
-                      month: "short",
-                      day: "numeric",
-                      timeZone: "UTC",
-                    };
-                    const formattedDate = new Intl.DateTimeFormat(
-                      "en-US",
-                      options
-                    ).format(dateObj);
+        <Table borderless striped className="p-3 shadow-sm rounded">
+          <tbody>
+            {errorMessage || forecasts.length < 1 ? (
+              <tr key={errorMessage}>
+                <td colSpan={2} className="text-break text-wrap">
+                  {errorMessage || `No weather data available`}
+                </td>
+              </tr>
+            ) : (
+              forecasts.map(({ date, description }) => {
+                const dateObj = new Date(date);
+                const options = {
+                  weekday: "short",
+                  month: "short",
+                  day: "numeric",
+                  timeZone: "UTC",
+                };
+                const formattedDate = new Intl.DateTimeFormat(
+                  "en-US",
+                  options
+                ).format(dateObj);
 
-                    return (
-                      <tr key={date + description}>
-                        <td className="text-break text-wrap">
-                          {formattedDate}
-                        </td>
-                        <td className="text-break text-wrap">{description}</td>
-                      </tr>
-                    );
-                  })
-                )}
-              </tbody>
-            </Table>
-          </Card.Body>
-        </Card>
+                return (
+                  <tr key={date + description}>
+                    <td className="text-break text-wrap">{formattedDate}</td>
+                    <td className="text-break text-wrap">{description}</td>
+                  </tr>
+                );
+              })
+            )}
+          </tbody>
+        </Table>
       </>
     );
   }
